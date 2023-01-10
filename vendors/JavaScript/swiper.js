b2b85b69e56f46aac001684be4e3b3be5095ea48
MIvanRais <26ivanrais@gmail.com>
2023-01-10 19:35:17 +0700

    Create The Sliders
    - The slider in recommended section
    - The slider in categories section

---
+++ vendors/JavaScript/swiper.js
@@ -0,0 +1,42 @@
+const swiper_recommended = new Swiper('.swiper_recommended', {
+    slidesPerView: 2,
+    spaceBetween: 15,
+    slidesPerGroup: 2,
+    loop: true,
+    centerSlide: 'true',
+    loopFillGroupWithBlank: false,
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+
+        },
+        576: {
+
+        }
+    }
+});
+
+const swiper_categories = new Swiper('.swiper_categories', {
+    slidesPerView: 2.9,
+    slidesPerGroup: 2,
+    loop: true,
+    centerSlide: 'true',
+    loopFillGroupWithBlank: false,
+    grabCursor: 'true',
+    breakpoints: {
+        0: {
+
+        },
+        576: {
+
+        }
+    }
+});
