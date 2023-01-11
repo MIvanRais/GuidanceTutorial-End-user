42de5d097279f979f3e0cca0530fb7536959406f
MIvanRais <26ivanrais@gmail.com>
2023-01-11 13:39:03 +0700

    Build Register Modal

--- resources/JavaScript/app.js
+++ resources/JavaScript/app.js
@@ -1,12 +1,29 @@
-//** modal-login
+//////** modal-login - modal-register
 const btnLoginForm = document.querySelector('#open-modal-login');
 const modalLogin = document.querySelector('#modal-login');
-const btnCloseLoginForm = document.querySelector('#close-modal-login');
+const modalRegister = document.querySelector('#modal-register');
+const closeModals = document.querySelectorAll('.close-modal');
+const openModals = document.querySelectorAll('.open-modal');
 
+//**show the modalLogin */
 btnLoginForm.addEventListener('click', () => {
     modalLogin.showModal();
 })
-
-btnCloseLoginForm.addEventListener('click', () => {
+//**close the modalLogin */
+closeModals[0].addEventListener('click', () => {
     modalLogin.close();
+})
+//**close the modalLogin and then show the modalRegister */
+openModals[0].addEventListener('click', () => {
+    modalLogin.close();
+    modalRegister.showModal();
+})
+//**close the modalRegister */
+closeModals[1].addEventListener('click', () => {
+    modalRegister.close();
+})
+//**close the modalRegister and then show the modalLogin */
+openModals[1].addEventListener('click', () => {
+    modalRegister.close();
+    modalLogin.showModal();
 })
