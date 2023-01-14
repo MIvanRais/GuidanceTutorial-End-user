92718bb8857ffe599f5fe62ffc14f65eba9e6f04
MIvanRais <26ivanrais@gmail.com>
2023-01-14 22:48:26 +0700

    Create a Slider in The video-lesson Page

--- vendors/JavaScript/swiper.js
+++ vendors/JavaScript/swiper.js
@@ -1,3 +1,4 @@
+// **Swiper on The Home Page
 const swiper_recommended = new Swiper('.swiper_recommended', {
     slidesPerView: 2,
     spaceBetween: 15,
@@ -31,6 +32,33 @@ const swiper_categories = new Swiper('.swiper_categories', {
     centerSlide: 'true',
     loopFillGroupWithBlank: false,
     grabCursor: 'true',
+    breakpoints: {
+        0: {
+
+        },
+        576: {
+
+        }
+    }
+});
+
+// **Swiper on The Video Lesson Page
+const swiper_next_video = new Swiper('.swiper_next_video', {
+    slidesPerView: 2,
+    spaceBetween: 15,
+    slidesPerGroup: 2,
+    loop: true,
+    centerSlide: 'true',
+    loopFillGroupWithBlank: false,
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
     breakpoints: {
         0: {
 
